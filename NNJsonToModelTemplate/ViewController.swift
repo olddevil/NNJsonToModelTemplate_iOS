3ea8a19a83f290ba36fb4ce0e3a01da37a2e7a83
olddevil <625158983@qq.com>
2022-10-05 00:48:45 +0800

    Initial Commit

---
+++ NNJsonToModelTemplate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NNJsonToModelTemplate
+//
+//  Created by olddevil on 2022/10/5.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
